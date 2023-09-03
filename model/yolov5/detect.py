908f6af27340b1638099b4fec11930118db7e709
Lorenzo-Cardoni <cardonilorenzo31@gmail.com>
2023-09-03 15:39:08 +0200

    Aggiunto calcolo del batch e tolta parte di pred

--- model/yolov5/detect.py
+++ model/yolov5/detect.py
@@ -43,14 +43,18 @@ if str(ROOT) not in sys.path:
 ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
 from models.common import DetectMultiBackend
-from utils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadImages, LoadScreenshots, LoadStreams
 from utils.general import (LOGGER, Profile, check_file, check_img_size, check_imshow, check_requirements, colorstr, cv2,
                            increment_path, non_max_suppression, print_args, scale_boxes, strip_optimizer, xyxy2xywh)
 from utils.plots import Annotator, colors, save_one_box
 from utils.torch_utils import select_device, smart_inference_mode
 import torch.utils.data
 import torchvision.transforms.functional as F
-from codecarbon import OfflineEmissionsTracker
+from torchvision import transforms, datasets
+import matplotlib.pyplot as plt
+import torchvision.utils as vutils
+import numpy as np
+
+# from codecarbon import EmissionsTracker
 
 @smart_inference_mode()
 def run(
@@ -83,13 +87,6 @@ def run(
         vid_stride=1,  # video frame-rate stride
 ):
     source = str(source)
-    save_img = not nosave and not source.endswith('.txt')  # save inference images
-    is_file = Path(source).suffix[1:] in (IMG_FORMATS + VID_FORMATS)
-    is_url = source.lower().startswith(('rtsp://', 'rtmp://', 'http://', 'https://'))
-    webcam = source.isnumeric() or source.endswith('.streams') or (is_url and not is_file)
-    screenshot = source.lower().startswith('screen')
-    if is_url and is_file:
-        source = check_file(source)  # download
 
     # Directories
     save_dir = increment_path(Path(project) / name, exist_ok=exist_ok)  # increment run
@@ -102,150 +99,65 @@ def run(
     imgsz = check_img_size(imgsz, s=stride)  # check image size
 
     # Dataloader
-    bs = 1  # batch_size
-    if webcam:
-        view_img = check_imshow(warn=True)
-        dataset = LoadStreams(source, img_size=imgsz, stride=stride, auto=pt, vid_stride=vid_stride)
-        bs = len(dataset)
-    elif screenshot:
-        dataset = LoadScreenshots(source, img_size=imgsz, stride=stride, auto=pt)
-    else:
-        dataset = LoadImages(source, img_size=imgsz, stride=stride, auto=pt, vid_stride=vid_stride)
-    vid_path, vid_writer = [None] * bs, [None] * bs
+    bs = 3  # batch_size
+
+    # Number of workers for dataloader
+    workers = 2
+
+    # Number of training epochs for GAN
+    num_epochs = 5
+
+    dataset = datasets.ImageFolder(source,
+                               transform=transforms.Compose([
+                               transforms.Resize(imgsz),
+                               transforms.CenterCrop(imgsz),
+                               transforms.ToTensor(),
+                               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                           ]))
+    
+    dataloader = torch.utils.data.DataLoader(dataset,
+                                         batch_size=bs,
+                                         shuffle=True, num_workers=workers)
 
     # Run inference
     model.warmup(imgsz=(1 if pt or model.triton else bs, 3, *imgsz))  # warmup
     seen, windows, dt = 0, [], (Profile(), Profile(), Profile())
 
-    # Batch size during training for GAN
-    batch_size = 128
 
-    # Number of training epochs for GAN
-    num_epochs = 5
-
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
-                                         shuffle=True)
-    
-    tracker = OfflineEmissionsTracker(country_iso_code="CAN") #codecarbon
-    tracker.start() #codecarbon
+    #tracker = EmissionsTracker(project_name="GAN-Training") #codecarbon
+    #tracker.start() #codecarbon
     for epoch in range(num_epochs):
         # For each batch in the dataloader
-        # for i, data in enumerate(dataloader, 0):
-            # dataset = data
-            for path, im, im0s, vid_cap, s in dataset:
+        for i, data in enumerate(dataloader, 0):
 
-                im2 = im # appoggio
-                counter = 0 # counter per il ciclo while
+            counter = 0 # counter per il ciclo while
 
-                while counter < 2: # inferenza per una immagine con dimensione 416x416 e 224x224
-                    im = im2
-                    with dt[0]:
-                        im = torch.from_numpy(im).to(model.device)
+            # inferenza per una immagine con dimensione 416x416 e 224x224
+            while counter < 2: 
+                with dt[0]:
+                    # Format batch
+                    im = data[0].to(model.device)
+                    b_size = im.size(0) #dimensione del batch
+                    print(b_size)
+                    if counter == 1: # eseguita l'inferenza per l'immagine 416x416 si ridimensiona il tensore a 224x224
+                        im = F.resize(im,224) # resize
 
-                        if counter == 1: # eseguita l'inferenza per l'immagine 416x416 si ridimensiona il tensore a 224x224
-                            im = F.resize(im,224) # resize
+                im = im.half() if model.fp16 else im.float()  # uint8 to fp16/32
+                im /= 255  # 0 - 255 to 0.0 - 1.0
+                if len(im.shape) == 3:
+                    im = im[None]  # expand for batch dim
 
-                        im = im.half() if model.fp16 else im.float()  # uint8 to fp16/32
-                        im /= 255  # 0 - 255 to 0.0 - 1.0
-                        if len(im.shape) == 3:
-                            im = im[None]  # expand for batch dim
-
-                    # Inference
-                    with dt[1]:
-                        visualize = increment_path(save_dir / Path(path).stem, mkdir=True) if visualize else False
-                        pred = model(im, augment=augment, visualize=visualize)
-
-                    # NMS
-                    with dt[2]:
-                        pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
-
-                    # Second-stage classifier (optional)
-                    # pred = utils.general.apply_classifier(pred, classifier_model, im, im0s)
-
-                    # Process predictions
-                    for i, det in enumerate(pred):  # per image
-                        seen += 1
-                        if webcam:  # batch_size >= 1
-                            p, im0, frame = path[i], im0s[i].copy(), dataset.count
-                            s += f'{i}: '
-                        else:
-                            p, im0, frame = path, im0s.copy(), getattr(dataset, 'frame', 0)
-
-                        p = Path(p)  # to Path
-                        save_path = str(save_dir / p.name)  # im.jpg
-                        txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # im.txt
-                        s += '%gx%g ' % im.shape[2:]  # print string
-                        gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
-                        imc = im0.copy() if save_crop else im0  # for save_crop
-                        annotator = Annotator(im0, line_width=line_thickness, example=str(names))
-                        if len(det):
-                            # Rescale boxes from img_size to im0 size
-                            det[:, :4] = scale_boxes(im.shape[2:], det[:, :4], im0.shape).round()
-
-                            # Print results
-                            for c in det[:, 5].unique():
-                                n = (det[:, 5] == c).sum()  # detections per class
-                                s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
-
-                            # Write results
-                            for *xyxy, conf, cls in reversed(det):
-                                if save_txt:  # Write to file
-                                    xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                                    line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
-                                    with open(f'{txt_path}.txt', 'a') as f:
-                                        f.write(('%g ' * len(line)).rstrip() % line + '\n')
-
-                                if save_img or save_crop or view_img:  # Add bbox to image
-                                    c = int(cls)  # integer class
-                                    label = None if hide_labels else (names[c] if hide_conf else f'{names[c]} {conf:.2f}')
-                                    annotator.box_label(xyxy, label, color=colors(c, True))
-                                if save_crop:
-                                    save_one_box(xyxy, imc, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg', BGR=True)
-
-                        # Stream results
-                        im0 = annotator.result()
-                        if view_img:
-                            if platform.system() == 'Linux' and p not in windows:
-                                windows.append(p)
-                                cv2.namedWindow(str(p), cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)  # allow window resize (Linux)
-                                cv2.resizeWindow(str(p), im0.shape[1], im0.shape[0])
-                            cv2.imshow(str(p), im0)
-                            cv2.waitKey(1)  # 1 millisecond
-
-                        # Save results (image with detections)
-                        if save_img:
-                            if dataset.mode == 'image':
-                                cv2.imwrite(save_path, im0)
-                            else:  # 'video' or 'stream'
-                                if vid_path[i] != save_path:  # new video
-                                    vid_path[i] = save_path
-                                    if isinstance(vid_writer[i], cv2.VideoWriter):
-                                        vid_writer[i].release()  # release previous video writer
-                                    if vid_cap:  # video
-                                        fps = vid_cap.get(cv2.CAP_PROP_FPS)
-                                        w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-                                        h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-                                    else:  # stream
-                                        fps, w, h = 30, im0.shape[1], im0.shape[0]
-                                    save_path = str(Path(save_path).with_suffix('.mp4'))  # force *.mp4 suffix on results videos
-                                    vid_writer[i] = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
-                                vid_writer[i].write(im0)
-
-                    # Print time (inference-only)
-                    LOGGER.info(f"{s}{'' if len(det) else '(no detections), '}{dt[1].dt * 1E3:.1f}ms")
-                    counter += 1
-    tracker.stop()
+                # Inference
+                with dt[1]:
+                    pred = model(im, augment=augment, visualize=visualize)
+                    #print(counter)
+                
+                # NMS
+                with dt[2]:
+                    pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
+                    counter += 1 #incremento del counter per eseguire l'inferenza sull'immagine 224x224 o alla prossima immagine
+    #tracker.stop()
     
-    # Print results
-    t = tuple(x.t / seen * 1E3 for x in dt)  # speeds per image
-    LOGGER.info(f'Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS per image at shape {(1, 3, *imgsz)}' % t)
-    if save_txt or save_img:
-        s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
-        LOGGER.info(f"Results saved to {colorstr('bold', save_dir)}{s}")
-    if update:
-        strip_optimizer(weights[0])  # update model (to fix SourceChangeWarning)
-
-
 def parse_opt():
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'yolov5s.pt', help='model path or triton URL')
